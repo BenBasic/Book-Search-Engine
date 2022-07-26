1f1c7d339a23e80e7898126c16b3c90c803409ab
BenBasic <benjaminmbasic@gmail.com>
2022-07-26 15:50:01 -0400

    Refactored and commented server.js to use Apollo Server, added and changed dependencies in package.json files

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
+// Setting up file requirements
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
+// Defining the database connection
+const db = require('./config/connection');
+
+// Defining the server port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Setting up the Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Initializing server
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +28,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+
+// Creates a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Calling function to start the server  while passing in typeDefs and resolvers
+startApolloServer(typeDefs, resolvers);
